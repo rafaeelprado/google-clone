ddefbddcf80ecac620e2971a335343f364a84d7a
Rafael Prado <rafapsd@gmail.com>
2022-06-08 18:35:01 -0300

    First commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo.svg';
+import { FaAdjust } from "react-icons/fa";
+
 
 import { Search } from './Search';
 
@@ -7,13 +10,12 @@ export const Navbar = ({darkTheme, setDarkTheme}) => {
   return (
     <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b border-gray-700 border-gray-200">
         <div className="flex justify-between items-center space-x-5 w-screen">
-            <Link to="/">
-                <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900">
-                  Eureka!  
-                  </p>           
+            <Link to="/">            
+              <img className="h-9 w9" src={logo} alt="logo"/>          
+                            
              </Link>
-             <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg">
-             {darkTheme ? 'Light' : 'Dark'}
+             <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-1 py-1 hover:shadow-lg">
+             {darkTheme ? <FaAdjust/> : <FaAdjust/>}
              </button>             
             </div>
             <Search />
